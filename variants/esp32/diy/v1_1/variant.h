d6672ab09282e2cf485d93cc59ad61915ea7d627
lenprog77 <lenprog.77@gmail.com>
2026-07-07 22:31:32 +0300

    Update variant.h

--- variants/esp32/diy/v1_1/variant.h
+++ variants/esp32/diy/v1_1/variant.h
@@ -10,26 +10,26 @@
 #define BUTTON_NEED_PULLUP
 #define EXT_NOTIFY_OUT 12 // Overridden default pin to use for Ext Notify Module (#975).
 
-#define LORA_DIO0 26  // a No connect on the SX1262/SX1268 module
-#define LORA_RESET 27 // RST for SX1276, and for SX1262/SX1268
-#define LORA_DIO1 33  // IRQ for SX1262/SX1268
-#define LORA_DIO2 32  // BUSY for SX1262/SX1268
+#define LORA_DIO0 33  // a No connect on the SX1262/SX1268 module
+#define LORA_RESET 19 // RST for SX1276, and for SX1262/SX1268
+#define LORA_DIO1 13  // IRQ for SX1262/SX1268
+#define LORA_DIO2 27  // BUSY for SX1262/SX1268
 #define LORA_DIO3     // Not connected on PCB, but internally on the TTGO SX1262/SX1268, if DIO3 is high the TXCO is enabled
 
-// In transmitting, set TXEN as high communication level，RXEN pin is low level;
+// In transmitting, set TXEN as high communication level,RXEN pin is low level;
 // In receiving, set RXEN as high communication level, TXEN is lowlevel;
-// Before powering off, set TXEN、RXEN as low level.
-#define LORA_RXEN 14 // Input - RF switch RX control, connecting external MCU IO, valid in high level
-#define LORA_TXEN 13 // Input - RF switch TX control, connecting external MCU IO or DIO2, valid in high level
+// Before powering off, set TXEN?RXEN as low level.
+#define LORA_RXEN 25 // Input - RF switch RX control, connecting external MCU IO, valid in high level
+#define LORA_TXEN 26 // Input - RF switch TX control, connecting external MCU IO or DIO2, valid in high level
 
 #undef LORA_SCK
-#define LORA_SCK 18
+#define LORA_SCK 5
 #undef LORA_MISO
-#define LORA_MISO 19
+#define LORA_MISO 14
 #undef LORA_MOSI
 #define LORA_MOSI 23
 #undef LORA_CS
-#define LORA_CS 5
+#define LORA_CS 18
 
 // RX/TX for RFM95/SX127x
 #define RF95_RXEN LORA_RXEN
@@ -37,7 +37,7 @@
 // #define RF95_TCXO <GPIO#>
 
 // common pinouts for SX126X modules
-#define SX126X_CS 5
+#define SX126X_CS 18
 #define SX126X_DIO1 LORA_DIO1
 #define SX126X_BUSY LORA_DIO2
 #define SX126X_RESET LORA_RESET
